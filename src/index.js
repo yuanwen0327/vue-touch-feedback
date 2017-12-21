e6b353c7f64bfcdfefcd2d51ddb361705189e9b4
yuanwen <yuanwen0327@outlook.com>
2017-12-21 23:40:08 +0800

    feat :解绑引用

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import TouchFeedback from './feedback'
 import defaults from 'lodash/defaults'
+
+let hashMap = []
+let count = 0
+
 export default {
     install(Vue, options = {}) {
-        Vue.directive('feedback', function (el,binding) {
-            new TouchFeedback(el,defaults(binding.value,options))
+        Vue.directive('feedback', {
+            bind(el, binding) {
+                hashMap[count] = new TouchFeedback(el, defaults(binding.value, options))
+                el.dataset.id = count
+                count++
+            },
+            unbind(el) {
+                hashMap[el.dataset.id].destory()
+                delete hashMap[el.dataset.id]
+            }
         })
     }
 }
